f67b77f23b1246dbbf8d49891e4bc6c1484edb40
tupsi07 <sanjida657@yahoo.com>
2025-05-13 18:50:59 +0600

    add controller for product routes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,22 +1,108 @@
-const addProduct =(req, res) =>{
-    
-}
+import ProductModel from "../models/Product.js";
 
-const listProducts =(req, res) =>{
+const addProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
 
-}
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image3 && req.files.image4[0];
 
-const removeProduct =(req, res) =>{
+    // if an image is not uploaded
+    const images = [image1, image2, image3, image4].filter(
+      (img) => img !== undefined,
+    );
 
-}
+    const imageUrl = await Promise.all(
+      images.map(async (img) => {
+        const result = await cloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      }),
+    );
 
-const singleProduct =(req, res) =>{
+    const prodeuctData = {
+      name,
+      description,
+      price: Number(price),
+      category,
+      subCategory,
+      sizes: JSON.parse(sizes),
+      bestseller: bestseller === "true",
+      date: Date.now(),
+    };
 
-}
+    const newProduct = await new ProductModel(prodeuctData);
+    await newProduct.save();
 
-export {
-    addProduct,
-    listProducts,
-    removeProduct,
-    singleProduct
-}
+    res.status(200).json({
+      success: true,
+      message: "Product added successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const listProducts = async (req, res) => {
+  try {
+    const products = await ProductModel.find({});
+
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const removeProduct = async (req, res) => {
+  try {
+    await ProductModel.findByIdAndDelete(req.body.id);
+    res.status(200).json({
+      success: true,
+      message: "Product removed successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await ProductModel.findById(productId);
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { addProduct, listProducts, removeProduct, singleProduct };
+
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,26 @@
-import express from 'express'
-import { addProduct, listProducts, removeProduct, singleProduct } from '../controllers/productController'
+import express from "express";
+import {
+    addProduct,
+    listProducts,
+    removeProduct,
+    singleProduct,
+} from "../controllers/productController.js";
+import upload from "../middleware/imageUpload.js";
 
-const productRoute = express.Router()
+const productRoute = express.Router();
 
-productRoute.post('/add', addProduct)
-productRoute.post('/remove', removeProduct )
-productRoute.post('/single', singleProduct )
-productRoute.get('/list', listProducts )
+productRoute.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct,
+);
+productRoute.post("/remove", removeProduct);
+productRoute.post("/single", singleProduct);
+productRoute.get("/list", listProducts);
 
 export default productRoute;
