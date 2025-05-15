c78587ee89438b65f136011e717f9626a0be619c
tupsi07 <sanjida657@yahoo.com>
2025-05-15 14:46:18 +0600

    style the admin layout

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Order from "./pages/Order";
 function App() {
+  const [token, setToken] = useState("");
   return (
     <div className="bg-gray-50 min-h-screen">
-      <Navbar />
-      <hr />
-      <div className="flex w-full">
-        <Sidebar />
-      </div>
-      <Routes>
-        <Route path="/add" element={<Add />} />
-        <Route path="/list" element={<List />} />
-        <Route path="/order" element={<Order />} />
-      </Routes>
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/order" element={<Order />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
