f5fbb1c88ff168d663fd12a73d6f15d79b7b9294
tupsi07 <sanjida657@yahoo.com>
2025-05-23 00:56:57 +0600

    add userAuth

---
+++ backend/middleware/userAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = (req, res, next) => {
+  const { token } = req.headers;
+  try {
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Not authorized login again",
+      });
+    }
+
+    const decoded_token = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded_token.id;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export default userAuth;
