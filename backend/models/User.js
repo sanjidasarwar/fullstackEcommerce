3fe3e62ec43a1007bc6a16896d9fb43ffcc7d259
tupsi07 <sanjida657@yahoo.com>
2025-05-24 13:12:17 +0600

    create order model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const userSchema =new mongoose.Schema({
-    name:{type: String, require:true},
-    email:{type:String, require:true, unique: true},
-    password:{type: String, require:true},
-    cartData:{type:Object, default:{}}
-}, {minimize:false})
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+    cartData: { type: Object, default: {} },
+  },
+  { minimize: false },
+);
 
-const UserModel =mongoose.models.user || mongoose.model('user', userSchema)
+const UserModel = mongoose.models.user || mongoose.model("user", userSchema);
 
-export default UserModel
+export default UserModel;
