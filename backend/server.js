a2fd4c18e8ca79ed630b54bf0b322dc540972e2c
tupsi07 <sanjida657@yahoo.com>
2025-06-01 03:02:56 +0600

    add origin in cors()

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,37 @@ connectCloudinary();
 
 const port = process.env.PORT || 8000;
 
+const allowedOrigins = [
+  "https://ecommerce-frontend-seven-wine.vercel.app",
+  "https://ecommerce-admin-yourproject.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+
 // middlewares
+// app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // api endpoint
-app.use("user", userRoute);
-app.use("product", productRoute);
-app.use("cart", cartRoute);
-app.use("order", orderRoute);
+app.use("/user", userRoute);
+app.use("/product", productRoute);
+app.use("/cart", cartRoute);
+app.use("/order", orderRoute);
 
 app.get("/", (req, res) => {
   res.send("Api working");
